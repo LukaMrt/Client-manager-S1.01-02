4659a738598e78ef9d9264a86af60dd5115bc366
Luka Maret <maret.luka@gmail.com>
2021-11-08 14:19:40 +0100

    fix: import bugs

--- src/commands/add/add.c
+++ src/commands/add/add.c
@@ -1,4 +1,4 @@
-#include <printf.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "../../data.h"
--- src/commands/load/load.h
+++ src/commands/load/load.h
@@ -1,6 +1,9 @@
 #ifndef CLIENT_MANAGER_S1_01_02_LOAD_H
 #define CLIENT_MANAGER_S1_01_02_LOAD_H
 
+#include "../../data.h"
+#include "../commands.h"
+
 void load(Customer *customer, Command command);
 
 #endif //CLIENT_MANAGER_S1_01_02_LOAD_H
