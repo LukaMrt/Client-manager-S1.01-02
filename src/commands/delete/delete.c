b41a685e61e609e736b3d23492e20763da85207b
Angelo Naudts <naudtsangelo@gmail.com>
2021-11-10 14:24:44 +0100

    feat: delete command v2
    fix: command delete

--- src/commands/delete/delete.c
+++ src/commands/delete/delete.c
@@ -10,74 +10,48 @@ bool compareStrings(const char name[20], const char name1[20]);
 void delete(Customer *customer, Command command) {
 
     char name[20];
-    int i, n = 0;
     printf("Enter the name of the customer to delete : ");
     fgets(name, 20, stdin);
 
-    Customer *next = customer;
+    Customer *current = customer;
     Customer *before = customer;
-    Customer *now;
     Customer *after;
-    Customer *last1;
-    Customer *last;
 
-    while (!compareStrings(next->name, name) && next->next != NULL && next->postalCode != -1) {
-        if (before != next) {
+    while (!compareStrings(current->name, name) && current->next != NULL && current->postalCode != -1) {
+        if (before != current) {
             before = before->next;
         }
-        next = next->next;
+        current = current->next;
     }
 
-    if (!compareStrings(next->name, name) && next->next == NULL) {
+    if (!compareStrings(current->name, name)) {
         printf("Customer not found.\n");
         return;
     }
 
-    now = next;
-    after = next->next;
-    while (next->next != NULL && next->postalCode != -1) {
-        next = next->next;
-        n += 1;
+    after = current->next;
+    before->next = after;
+
+    Customer newCustomer = {
+            .name = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+            .surname = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+            .city = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+            .postalCode = -1,
+            .phone = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+            .email = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+            .job = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+            .next = NULL
+    };
+
+    if (after == NULL) {
+        after = &newCustomer;
     }
 
-    last = next;
-    next = now;
-    for (i = 0; i < n - 1; i++) {
-        next = next->next;
-    }
-    last1 = next;
-
-    if (last == now) {
-        last1 = before;
+    if (before == current) {
+        *customer = *after;
     }
 
-    before->next = last;
-    last->next = after;
-    last1->next = NULL;
-
-    if (last1 == now) {
-        last->next = NULL;
-    }
-
-    if (now == customer) {
-        *customer = *last;
-    }
-
-    if (before == last) {
-        Customer newCustomer = {
-                "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                -1,
-                "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                NULL
-        };
-        *customer = newCustomer;
-    }
-
-    printf("Customer successfully deleted.\n");
+    printf("Customer deleted successfully.\n");
 }
 
 bool compareStrings(const char name[20], const char name1[20]) {
