3d78d3e91a76924df62df3893c1c2c53bea39ba1
Angelo Naudts <angelo.naudts@etu.univ-lyon1.fr>
2021-12-14 08:45:41 +0100

    feat: help file

--- src/commands/help/help.c
+++ src/commands/help/help.c
@@ -22,8 +22,7 @@ void help() {
     printf("\t-field, -f : specify the field which will be used for the deletion.\n");
     printf("\t-value, -v : specify the value of the field used for the deletion.\n");
 
-    printf("\nCommand edit : edit a user in the customers list.\n");
-    printf("\t-id, -i : specify the id of the customer to edit (in the list displayed by show command).\n");
+    printf("\nCommand edit : edit all user matching with given regex in the customers list.\n");
     printf("\t-name, -n : specify the new name of the customer.\n");
     printf("\t-surname, -s : specify the new surname of the customer.\n");
     printf("\t-city, -c : specify the new city of the customer.\n");
@@ -31,8 +30,22 @@ void help() {
     printf("\t-phone, -ph : specify the new phone of the customer.\n");
     printf("\t-email, -e : specify the new email of the customer.\n");
     printf("\t-job, -j : specify the new job of the customer.\n");
+    printf("\t-sname, -sn : specify the name of the customer to search (regex).\n");
+    printf("\t-ssurname, -ss : specify the surname of the customer to search (regex).\n");
+    printf("\t-scity, -sc : specify the city of the customer to search (regex).\n");
+    printf("\t-spostal, -spostalCode, -spo : specify the postal code of the customer to search (regex).\n");
+    printf("\t-sphone, -sph : specify the phone of the customer to search (regex).\n");
+    printf("\t-semail, -se : specify the email of the customer to search (regex).\n");
+    printf("\t-sjob, -sj : specify the job of the customer to search (regex).\n");
 
     printf("\nCommand filter : filter customers list by a specific field.\n");
+    printf("\t-name, -n : specify the regex of the name of the customer.\n");
+    printf("\t-surname, -s : specify the regex of the surname of the customer.\n");
+    printf("\t-city, -c : specify the regex of the city of the customer.\n");
+    printf("\t-postal, -postalCode, -po : specify the regex of the postal code of the customer.\n");
+    printf("\t-phone, -ph : specify the regex of the phone of the customer.\n");
+    printf("\t-email, -e : specify the regex of the email of the customer.\n");
+    printf("\t-job, -j : specify the regex of the job of the customer.\n");
 
     printf("\nCommand help : display the commands documentation\n");
 
@@ -42,15 +55,17 @@ void help() {
     printf("\nCommand save : save customers list to a file.\n");
     printf("\t-file, -f : specify the path of the file in which customers will be saved.\n");
 
-    printf("\nCommand search : search a specific customers from fields values.\n");
-
     printf("\nCommand show : display the list of the customers.\n");
     printf("\t-filter, -f : specify the field which will be used to filter the list before display it (without delete users from the list).\n");
-    printf("\t-filter, -f : specify the field which will be used to sort the list before display it (without modify the list).\n");
+    printf("\t-sort, -s : specify the field which will be used to sort the list before display it (without modify the list).\n");
+    printf("\t-value, -v : specify the value of the field used for the filter.\n");
+    printf("\t-revert, -r : sort in the descending order.\n");
     printf("\t-complete, -c : show only customers without missing data.\n");
     printf("\t-incomplete, -i : show only customers with missing data.\n");
 
     printf("\nCommand sort : sort customers list by a specific field.\n");
+    printf("\t-field, -f : spectify the field that you want to sort.\n");
+    printf("\t-revert, -r : sort in the descending order.\n");
 
     printf("\nCommands exit, quit, stop, end, q : exit the program.\n");
 }
