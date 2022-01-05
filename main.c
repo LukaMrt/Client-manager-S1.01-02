f3df3009f86b2c8d50ebf2958e18aa66d82149a5
Luka Maret <maret.luka@gmail.com>
2022-01-05 10:33:34 +0100

    feat: add operations time

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "src/commands/commands.h"
 #include "src/utils/utils.h"
 #include "src/utils/customersUtils.h"
+
+long timeDifference(struct timespec *start, struct timespec *end);
 
 /**
  * Entry point of application
@@ -18,12 +21,22 @@ int main() {
     char *entry;
     Customer *customer = (Customer *) malloc(sizeof(Customer));
     createCustomer(customer);
+    struct timespec startTime;
+    struct timespec endTime;
 
     do {
 
         printf("\n => Enter a command : ");
         entry = scanString();
+
+        clock_gettime(CLOCK_REALTIME, &startTime);
         executeCommand(entry, customer);
+        clock_gettime(CLOCK_REALTIME, &endTime);
+
+        printf("Last operation's time : %lds %ldms %ldns\n",
+               timeDifference(&startTime, &endTime) / 1000000,
+               timeDifference(&startTime, &endTime) % 1000000 / 1000,
+               timeDifference(&startTime, &endTime) % 1000);
 
     } while (!compareStrings(entry, "exit")
              && !compareStrings(entry, "quit")
@@ -34,4 +47,16 @@ int main() {
     printf("\n  ~~~ Customers manager ~~~ \n");
 
     return 0;
+}
+
+/**
+ * Computes the time difference between two times
+ * @param start the start time
+ * @param end the end time
+ * @return the time difference in nanoseconds
+ */
+long timeDifference(struct timespec *start, struct timespec *end) {
+    long startTime = start->tv_sec * 1000000 + start->tv_nsec / 1000;
+    long endTime = end->tv_sec * 1000000 + end->tv_nsec / 1000;
+    return endTime - startTime;
 }
